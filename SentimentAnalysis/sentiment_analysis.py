98ffb5013e6eaf1ed53b26da69177a627c79fa8e
codertron3000 <judah.purcell@gmail.com>
2024-03-30 00:09:50 -0400

    project complete

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,6 +6,10 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json=myobj, headers=header)
+    if response.status_code == 500:
+        label = None
+        score = None
+        return {'label': label, 'score': score}
     formatted_response = json.loads(response.text)
     label = formatted_response['documentSentiment']['label']
     score = formatted_response['documentSentiment']['score']
--- server.py
+++ server.py
@@ -17,24 +17,24 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
     text_to_analyze = request.args.get('textToAnalyze')
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
-    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is not None:
+        return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
+    return "Your entry was bananas!"
 
-    
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
-    app.run(host='0.0.0.0', port=5000)
+    '''
+    app.run(host='0.0.0.0', port=3001)
+    
